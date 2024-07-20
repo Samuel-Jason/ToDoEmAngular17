c901b04f6ef7bc35f22a487c3a48bf031c6c5562
Samuel <samuelljason@gmail.com>
2024-07-20 17:27:34 -0300

    implementado o armazenamento no localStoragee depois feito o metodo que mostra as info na tela.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,57 +2,81 @@ import { RouterOutlet } from '@angular/router';
 import { Todo } from '../models/todo.model';
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ReactiveFormsModule,],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  schemas: [NO_ERRORS_SCHEMA]
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppComponent {
-  public todos: Todo[] =	[];
+  public todos: Todo[] = [];
   public menutitles: String = 'Minhas tarefas';
   public form: FormGroup;
 
   constructor(private fb: FormBuilder) {
-
     this.form = this.fb.group({
-      title: ['',
+      title: [
+        '',
         Validators.compose([
           Validators.minLength(4),
           Validators.maxLength(11),
           Validators.required,
-      ])]
+        ]),
+      ],
     });
+
+    this.load();
   }
 
-  add(){
+  add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
     this.clear();
   }
 
-  clear(){
+  clear() {
     this.form.reset();
   }
 
-  remove(todo: Todo){
+  remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
-    if(index != -1){
+    if (index != -1) {
       this.todos.splice(index, 1);
     }
   }
 
-  markAsDone(todo: Todo){
+  markAsDone(todo: Todo) {
     todo.done = true;
   }
 
-  markAsUndone(todo : Todo){
+  markAsUndone(todo: Todo) {
     todo.done = false;
   }
 
+  //METODO QUE SALVAM NO LOCALSTORAGE E MOSTRA NA TELA
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data !== null) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = []; // Inicializa this.todos como um array vazio se não houver dados salvos
+      console.log('Não há dados salvos para "todos" no localStorage.');
+    }
+  }
 }
